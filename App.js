32c538f9d94e111eaf1f6e0470bcc3f1b606f8d4
Carlos Habib <43607843+CarHab@users.noreply.github.com>
2019-07-26 20:03:33 -0300

    Update App.js

--- App.js
+++ App.js
@@ -39,13 +39,13 @@ export default class App extends Component {
 	componentDidMount() {
 		if (!firebase.apps.length) {
 			firebase.initializeApp({
-				apiKey: 'AIzaSyAZqkLlW64Fb2cLkOqVVjZy6PKGIC04k20',
-				authDomain: 'personalclub-23861.firebaseapp.com',
-				databaseURL: 'https://personalclub-23861.firebaseio.com',
-				projectId: 'personalclub-23861',
+				apiKey: '',
+				authDomain: '',
+				databaseURL: '',
+				projectId: '',
 				storageBucket: '',
-				messagingSenderId: '116547347880',
-				appId: '1:116547347880:web:2dd1c5c71983feb8'
+				messagingSenderId: '',
+				appId: ''
 			})
 			BackHandler.addEventListener('hardwareBackPress', () => BackHandler.exitApp())
 		}
